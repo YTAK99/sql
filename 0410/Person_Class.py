7707e56d955cdb1278d859484696add2d6112d57
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-10 20:44:28 +0900

    Update Person_Class.py

--- 0410/Person_Class.py
+++ 0410/Person_Class.py
@@ -11,3 +11,12 @@ class Person:                   # 클래스 - 속성, 메소드
 
     def info(self):
         print(f"사원번호:{self.empno}   사원명:{self.ename}   직업:{self.job}   사수번호:{self.mgr}   고용일:{self.hiredate}   급여:{self.sal}   보너스:{self.comm}   부서번호:{self.deptno}")
+
+
+'''
+    def print_person(self):
+        if self.sal is not None:
+            print(f"{self.empno} : {self.ename:>10} : {int(self.sal):>7}")
+        else:
+            print(f"{self.empno} : {self.ename:>10} :       0")
+'''
