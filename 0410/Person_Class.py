da49a43b15b269730653bd2403ef8541d167a76a
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-10 16:20:57 +0900

    Create Person_Class.py

---
+++ 0410/Person_Class.py
@@ -0,0 +1,13 @@
+class Person:                   # 클래스 - 속성, 메소드
+    def __init__(self, empno, ename, job, mgr, hiredate, sal, comm, deptno):
+        self.empno = empno
+        self.ename = ename
+        self.job = job
+        self.mgr = mgr
+        self.hiredate = hiredate
+        self.sal = int(sal)
+        self.comm = comm
+        self.deptno = deptno
+
+    def info(self):
+        print(f"사원번호:{self.empno}   사원명:{self.ename}   직업:{self.job}   사수번호:{self.mgr}   고용일:{self.hiredate}   급여:{self.sal}   보너스:{self.comm}   부서번호:{self.deptno}")
