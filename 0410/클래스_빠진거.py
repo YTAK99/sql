aa05318bcb0d05e6d3d5f7b461418b4f6930e833
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-10 16:17:04 +0900

    Create 클래스_빠진거.py

---
+++ 0410/클래스_빠진거.py
@@ -0,0 +1,123 @@
+import oracledb
+from person import Person
+
+# 데이터베이스 접속 정보 설정
+dsn = oracledb.makedsn("localhost", 1521, service_name="XE")
+conn = oracledb.connect(user="c##mbc", password="qwer1234", dsn=dsn)
+
+# 쿼리 실행을 위한 커서 생성
+cursor = conn.cursor()
+
+PList = []      # Person DB 리스트 저장
+
+def show_menu():
+    print("-- 임직원 관리 시스템 --")
+    print("- 1. 직원 추가    -")
+    print("- 2. 직원 삭제    -")
+    print("- 3. 직원 조회    -")
+    print("- 4. 프로그램 종료 -")
+    menu_num = input("메뉴를 선택해 주세요: ")
+    print(menu_num)
+    return menu_num
+
+def insert_emp(): # empno, ename, job, mgr, hiredate, sal, comm, deptno  
+    print("새로운 직원의 사번, 이름을 입력하세요....")
+    empno, ename = input().split()
+    print(empno, ename)
+
+    if empno.isdigit():        
+#INSERT 예제
+        try:
+            # INSERT INTO EMP(EMPNO, ENAME) VALUES('1234','LEO')
+            cursor.execute("INSERT INTO EMP(EMPNO, ENAME) VALUES (:1, :2)", [empno, ename.upper()])
+            conn.commit()
+            print("Data inserted successfully")
+        except oracledb.DatabaseError as e:
+            print(f"Error inserting data: {e}")
+    else:
+        print("ERR-INSERT-001 : 사번 입력 오류 입니다. 숫자만 입력 가능합니다.")
+
+
+def delete_emp():
+    print("삭제할 직원의 사번을 입력하세요....")
+    empno = input()
+    print(empno)
+
+    if len(PList) > 0:
+        eq = False
+        for i in PList:
+            if str(i.empno) == empno:
+                eq = True
+
+        if eq == True:
+            print("해당 사번이 존재합니다.")
+        else:
+            print('해당 사번이 존재하지 않습니다.')
+
+    else:
+        search_emp()
+    
+    # if empno.isdigit(): 
+    #     try:
+    #         cursor.execute("DELETE FROM EMP(EMPNO, ENAME) VALUES (:1, :2)", [empno, ename.upper()])
+    #         for row in cursor:
+    #             print(row)
+    #     except oracledb.DatabaseError as e:
+    #         print(f"Error fetching data: {e}")
+    # else:
+    #     print("ERR-DELETE-001 : 사번 입력 오류 입니다. 숫자만 입력 가능합니다.")
+
+
+def search_emp():
+    print("FUNCTION CALL search_emp()")
+# SELECT 예제
+    try:
+        cursor.execute('''
+                        SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+                        FROM emp 
+                        ORDER BY EMPNO
+                        ''')
+        for row in cursor:
+            p = Person(*row[:8])                # row의 0번부터 7번 인덱스까지 슬라이싱한 후 언패킹
+                                                # 동일 표현1 : p = Person(*(row[x] for x in range(8)))
+                                                # 동일 표현2 : p = Person(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+            PList.append(p)
+            
+        for i in PList:
+            i.info()
+            
+    except oracledb.DatabaseError as e:
+        print(f"Error fetching data: {e}")
+
+def search_emp2():
+    print("FUNCTION CALL search_emp2()")
+    for i in PList:
+        i.info()
+
+loop = True
+while loop:
+    select = int(show_menu())
+    if select == 1:
+        print("1. 직원 추가 메뉴")
+        insert_emp()
+
+    elif select == 2:
+        print("2. 직원 삭제 메뉴")
+        delete_emp()
+
+    elif select == 3:
+        print("3. 직원 조회 메뉴")
+        print("=== LST SIZE ===", len(PList))
+        if len(PList) == 0:
+            search_emp()
+        else:
+            search_emp2()
+
+    else:
+        print("*** 프로그램 종료 ***")
+        loop = False                    # break 써도 됨
+
+# 커서 및 커넥션 닫기
+cursor.close()
+conn.close()
+
